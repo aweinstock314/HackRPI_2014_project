bd25c49e81f1ee3caf44c8bb1242e8430c865729
Avi Weinstock <aweinstock314@gmail.com>
2014-11-15 18:09:30 -0500

    Added more detail to the client-server protocol, and a tcp handler that shows example messages. Made the TCP listener listen to external IP addresses (via use of 0.0.0.0 instead of 127.0.0.1).

--- server/src/main.rs
+++ server/src/main.rs
@@ -3,31 +3,80 @@ use serialize::json;
 use std::io::{TcpListener, TcpStream};
 use std::io::{Acceptor, Listener};
 
-pub type PlayerId = int;
 
 #[deriving(Encodable, Decodable)]
-pub enum Command {
-    MoveForward(PlayerId, f64),
-    MoveSideways(PlayerId, f64),
+//pub struct Position { x: f64, y: f64, z: f64 }
+pub struct Position (f64, f64, f64);
+
+#[deriving(Encodable, Decodable)]
+//pub struct Orientation { theta: f64, phi: f64 }
+pub struct Orientation (f64, f64);
+
+#[deriving(Encodable, Decodable)]
+pub enum PlayerCommand {
+    MoveForward(f64),
+    MoveSideways(f64),
+    RotateCamera(Orientation),
+    Shoot,
 }
 
 #[deriving(Encodable, Decodable)]
-pub struct Message {
-    command: Command,
+pub enum ObjectType {
+    Floor,
+    Obstacle(int),
+    Player,
+    Bullet,
+}
+
+#[deriving(Encodable, Decodable)]
+pub enum ServerCommand {
+    SetPosition(int, Position),
+    SetOrientation(int, Orientation),
+    AddObject(int, Position, Orientation, ObjectType),
+    RemoveObject(int),
+}
+
+#[deriving(Encodable, Decodable)]
+pub struct IncomingMessage {
+    command: PlayerCommand,
     timestamp: u64,
 }
-    
 
-// contains some code adapted from example at http://doc.rust-lang.org/std/io/net/tcp/struct.TcpListener.html
-
-fn handle_client(mut stream: TcpStream) {
-    println!("Received a connection from {}.", stream.peer_name());
-    stream.write_line("Hello, world!");
-    stream.write_line(json::encode(&Message { command: MoveForward(0, 0.5), timestamp: 0 }).as_slice());
+#[deriving(Encodable, Decodable)]
+pub struct OutgoingMessage {
+    command: ServerCommand,
+    timestamp: u64,
 }
 
+fn example_playercommands() -> Vec<PlayerCommand> { vec!(
+    MoveForward(0.5),
+    MoveSideways(0.6),
+    RotateCamera(Orientation(-1.5, 0.2)),
+    Shoot,
+)}
+
+fn example_servercommands() -> Vec<ServerCommand> { vec!(
+    SetPosition(42, Position(50.0, 10.0, 25.0)),
+    SetOrientation(42, Orientation(0.0, 0.0)),
+    AddObject(42, Position(0.0, 0.0, 0.0), Orientation(0.0, 0.0), Player),
+    RemoveObject(42),
+)}
+
+fn show_examples(mut stream: TcpStream) {
+    println!("Received a connection from {}.", stream.peer_name());
+    //stream.write_line(json::encode(&IncomingMessage{command: MoveForward(0.5), timestamp: 0}).as_slice());
+    for &cmd in example_servercommands().iter() {
+        stream.write_line(json::encode(&OutgoingMessage{command: cmd, timestamp: 0}).as_slice());
+    }
+    for &cmd in example_playercommands().iter() {
+        stream.write_line(json::encode(&IncomingMessage{command: cmd, timestamp: 0}).as_slice());
+    }
+}
+
+// contains some code adapted from example at http://doc.rust-lang.org/std/io/net/tcp/struct.TcpListener.html
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:51701"); //large number for port chosen pseudorandomly
+    //let listener = TcpListener::bind("127.0.0.1:51701"); //large number for port chosen pseudorandomly
+    let listener = TcpListener::bind("0.0.0.0:51701"); //large number for port chosen pseudorandomly
 
     let mut acceptor = listener.listen();
     
@@ -35,9 +84,8 @@ fn main() {
         match stream {
             Err(e) => { println!("Error accepting incoming connection: {}", e) }
             Ok(stream) => spawn(proc() {
-                handle_client(stream)
+                show_examples(stream)
             })
         }
     }
-    println!("Hello, world!");
 }
