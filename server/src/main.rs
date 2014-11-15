a6f584fa9e72520872e5444c72683c360d2c47ef
Avi Weinstock <aweinstock314@gmail.com>
2014-11-15 16:21:05 -0500

    Added some JSON stuff to the server.

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,10 +1,29 @@
+extern crate serialize;
+use serialize::json;
 use std::io::{TcpListener, TcpStream};
 use std::io::{Acceptor, Listener};
+
+pub type PlayerId = int;
+
+#[deriving(Encodable, Decodable)]
+pub enum Command {
+    MoveForward(PlayerId, f64),
+    MoveSideways(PlayerId, f64),
+}
+
+#[deriving(Encodable, Decodable)]
+pub struct Message {
+    command: Command,
+    timestamp: u64,
+}
+    
 
 // contains some code adapted from example at http://doc.rust-lang.org/std/io/net/tcp/struct.TcpListener.html
 
 fn handle_client(mut stream: TcpStream) {
+    println!("Received a connection from {}.", stream.peer_name());
     stream.write_line("Hello, world!");
+    stream.write_line(json::encode(&Message { command: MoveForward(0, 0.5), timestamp: 0 }).as_slice());
 }
 
 fn main() {
