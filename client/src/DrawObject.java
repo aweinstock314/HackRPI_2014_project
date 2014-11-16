69935379b5de2b53acfb7f0a94c8faeec72a2c60
breadknock <brenoc@rpi.edu>
2014-11-16 03:01:17 -0500

    Added drawing framework

--- client/src/DrawObject.java
+++ client/src/DrawObject.java
@@ -1,5 +1,8 @@
 package client;
 
+import javax.media.opengl.GL2;
+import org.json.simple.*;
+
 public class DrawObject {
     public double x;
     public double y;
@@ -7,6 +10,7 @@ public class DrawObject {
     public double theta;
     public double phi;
     public String type;
+    public boolean isPlayer;
 
     public DrawObject(double x, double y,double z, double th, double ph,String type) {
         setPosition(x,y,z);
@@ -25,4 +29,12 @@ public class DrawObject {
         phi = ph;
     }
 
+    public void draw(GL2 gl2, JSONArray model) {
+        if(isPlayer) return;
+        gl2.glBegin(gl2.GL_TRIANGLES);
+        for(int i = 0; i < (model.size()/3); i++) {
+            gl2.glVertex3d(((Number)model.get(i)).floatValue() + x, ((Number)model.get(i+1)).floatValue() + y, ((Number)model.get(i+2)).floatValue() + z);
+        }
+        gl2.glEnd();
+    }
 }
