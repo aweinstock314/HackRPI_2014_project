6b0f2ce91e58237d8c3b2852d6366c71099f9102
Avi Weinstock <aweinstock314@gmail.com>
2014-11-15 23:43:39 -0500

    Added a construct similar in spirit to the Template Method Design Pattern to allow construction of new JOGL windows with less boilerplate.

---
+++ client/src/AbstractGLWindow.java
@@ -0,0 +1,37 @@
+package client;
+
+import javax.media.opengl.GLEventListener;
+import javax.media.opengl.awt.GLCanvas;
+import javax.media.opengl.GLAutoDrawable;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public abstract class AbstractGLWindow extends JPanel implements GLEventListener
+{
+    protected static final int WIDTH = 640;
+    protected static final int HEIGHT = 480;
+
+    abstract public void display(GLAutoDrawable drawable);
+    abstract public void displayChanged(GLAutoDrawable drawable, boolean modeChanged, boolean deviceChanged);
+    abstract public void init(GLAutoDrawable drawable);
+    abstract public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height);
+    abstract public void dispose(GLAutoDrawable drawable);
+
+    protected void constructorAux(int w, int h)
+    {
+        setLayout(null);
+        GLCanvas glcanv = new GLCanvas();
+        add(glcanv);
+        glcanv.setBounds(0, 0, w, h);
+    }
+    public static void do_main(AbstractGLWindow aglw)
+    {
+        JFrame jf = new JFrame();
+        jf.setSize(WIDTH, HEIGHT);
+        jf.setResizable(false);
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jf.setLayout(null);
+        jf.add(aglw);
+        jf.setVisible(true);
+    }
+}
--- client/src/ModelViewer.java
+++ client/src/ModelViewer.java
@@ -3,37 +3,15 @@ package client;
 import com.jogamp.opengl.util.FPSAnimator;
 import javax.media.opengl.GL2;
 import javax.media.opengl.GLAutoDrawable;
-import javax.media.opengl.GLEventListener;
-import javax.media.opengl.awt.GLCanvas;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
 
-public class ModelViewer extends JPanel implements GLEventListener
+public class ModelViewer extends AbstractGLWindow
 {
-    private static final int WIDTH = 640;
-    private static final int HEIGHT = 480;
-
-    public void display(GLAutoDrawable drawable){}
-    public void displayChanged(GLAutoDrawable drawable, boolean modeChanged, boolean deviceChanged){}
-    public void init(GLAutoDrawable drawable){}
-    public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height){}
+    public void display(GLAutoDrawable drawable) {}
+    public void displayChanged(GLAutoDrawable drawable, boolean modeChanged, boolean deviceChanged) {}
+    public void init(GLAutoDrawable drawable) {}
+    public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {}
     public void dispose(GLAutoDrawable drawable) {}
-    public ModelViewer(int w, int h)
-    {
-        setLayout(null);
-        GLCanvas glcanv = new GLCanvas();
-        add(glcanv);
-        glcanv.setBounds(0, 0, w, h);
-    }
-    public static void main(String[] args)
-    {
-        JFrame jf = new JFrame();
-        ModelViewer mv = new ModelViewer(WIDTH, HEIGHT);
-        jf.setSize(WIDTH, HEIGHT);
-        jf.setResizable(false);
-        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        jf.setLayout(null);
-        jf.add(mv);
-        jf.setVisible(true);
-    }
+
+    public ModelViewer(int w, int h) { constructorAux(w, h); }
+    public static void main(String[] args) { do_main(new ModelViewer(WIDTH, HEIGHT)); }
 }
