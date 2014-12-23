bf21bf80e7a77ba3f775dde34d3bfb32699638ec
Avi Weinstock <aweinstock314@gmail.com>
2014-12-23 16:22:15 -0500

    Made changes to the server to make it compile under the most recent nightly build of rust.

--- server/src/main.rs
+++ server/src/main.rs
@@ -4,7 +4,7 @@ use serialize::json;
 use std::io::{TcpListener, TcpStream, BufferedStream};
 use std::io::{Acceptor, Listener};
 use std::collections::HashMap;
-use std::collections::hash_map::{Vacant, Occupied};
+use std::collections::hash_map::Entry::{Vacant, Occupied};
 use std::sync::{Mutex, Arc};
 use std::num::FloatMath;
 use std::ops::Add;
@@ -16,30 +16,31 @@ static TAU: f64 = std::f64::consts::PI_2;
 #[link(name = "ode")] extern {}
 mod ode_bindgen;
 
-#[deriving(Encodable, Decodable, Clone, Show)]
+#[deriving(Encodable, Decodable, Clone, Show, Copy)]
 //pub struct Position { x: f64, y: f64, z: f64 }
 pub struct Position (f64, f64, f64);
 
 impl Add<Position, Position> for Position {
-    fn add(&self, other: &Position) -> Position {
-        let Position(x1, y1, z1) = *self;
-        let Position(x2, y2, z2) = *other;
+    fn add(self, other: Position) -> Position {
+        let Position(x1, y1, z1) = self;
+        let Position(x2, y2, z2) = other;
         Position(x1 + x2, y1 + y2, z1 + z2)
     }
 }
-#[deriving(Encodable, Decodable, Clone, Show)]
+//:%s/\(En\|De\)codable/Rustc\0/gc
+#[deriving(Encodable, Decodable, Clone, Show, Copy)]
 //pub struct Orientation { theta: f64, phi: f64 }
 pub struct Orientation (f64, f64);
 
 impl Add<Orientation, Orientation> for Orientation {
-    fn add(&self, other: &Orientation) -> Orientation {
-        let Orientation(t1, p1) = *self;
-        let Orientation(t2, p2) = *other;
+    fn add(self, other: Orientation) -> Orientation {
+        let Orientation(t1, p1) = self;
+        let Orientation(t2, p2) = other;
         Orientation(t1+t2, p1+p2)
     }
 }
 
-#[deriving(Encodable, Decodable, Clone)]
+#[deriving(Encodable, Decodable, Clone, Copy)]
 pub enum PlayerCommand {
     MoveForward(f64),
     MoveSideways(f64),
@@ -48,7 +49,7 @@ pub enum PlayerCommand {
     Shoot,
 }
 
-#[deriving(Encodable, Decodable, Clone)]
+#[deriving(Encodable, Decodable, Clone, Copy)]
 pub enum ObjectType {
     Floor,
     Obstacle(int),
@@ -56,7 +57,7 @@ pub enum ObjectType {
     Bullet,
 }
 
-#[deriving(Encodable, Decodable, Clone)]
+#[deriving(Encodable, Decodable, Clone, Copy)]
 pub enum ServerCommand {
     SetPosition(int, Position),
     SetOrientation(int, Orientation),
@@ -87,23 +88,23 @@ pub struct GameObject {
 }*/
 
 fn example_playercommands() -> Vec<PlayerCommand> { vec!(
-    MoveForward(0.5),
-    MoveSideways(0.6),
-    RotateCamera(Orientation(-1.5, 0.2)),
-    Shoot,
+    PlayerCommand::MoveForward(0.5),
+    PlayerCommand::MoveSideways(0.6),
+    PlayerCommand::RotateCamera(Orientation(-1.5, 0.2)),
+    PlayerCommand::Shoot,
 )}
 
 fn example_servercommands() -> Vec<ServerCommand> { vec!(
-    SetPosition(42, Position(50.0, 10.0, 25.0)),
-    SetOrientation(42, Orientation(0.0, 0.0)),
-    AddObject(42, Position(0.0, 0.0, 0.0), Orientation(0.0, 0.0), Player),
-    RemoveObject(42),
+    ServerCommand::SetPosition(42, Position(50.0, 10.0, 25.0)),
+    ServerCommand::SetOrientation(42, Orientation(0.0, 0.0)),
+    ServerCommand::AddObject(42, Position(0.0, 0.0, 0.0), Orientation(0.0, 0.0), ObjectType::Player),
+    ServerCommand::RemoveObject(42),
 )}
 
 fn show_examples(mut stream: TcpStream, playernum: int, transmit_playmove: Sender<(int, PlayerCommand)>) {
     let seconds = time::get_time().sec;
     println!("Received a connection from {} at time {} (player {}).", stream.peer_name(), seconds, playernum);
-    //stream.write_line(json::encode(&IncomingMessage{command: MoveForward(0.5), timestamp: 0}).as_slice());
+    //stream.write_line(json::encode(&IncomingMessage{command: PlayerCommand::MoveForward(0.5), timestamp: 0}).as_slice());
     for &cmd in example_servercommands().iter() {
         stream.write_line(json::encode(&OutgoingMessage{command: cmd, timestamp: seconds}).as_slice());
     }
@@ -118,7 +119,7 @@ fn interact_with_client(mut stream: TcpStream,
                         transmit_playmove: Sender<(int, PlayerCommand)>) {
     println!("Player #{} joined ({}).", playernum, stream.peer_name());
     let mut buffered = BufferedStream::new(stream.clone());
-    spawn(proc() { process_input_from_client(buffered, playernum, transmit_playmove) });
+    spawn(move || { process_input_from_client(buffered, playernum, transmit_playmove) });
     process_output_to_client(stream, playernum, receive_broadcast);
 }
 
@@ -141,7 +142,7 @@ fn process_input_from_client(mut stream: BufferedStream<TcpStream>,
 fn process_output_to_client(mut stream: TcpStream,
                             playernum: int,
                             receive_broadcast: Receiver<ServerCommand>) {
-    stream.write_line(json::encode(&SetPlayerNumber(playernum)).as_slice());
+    stream.write_line(json::encode(&ServerCommand::SetPlayerNumber(playernum)).as_slice());
     for action in receive_broadcast.iter() {
         stream.write_line(json::encode(&action).as_slice());
     }
@@ -161,7 +162,7 @@ fn get_player(world: &mut HashMap<int, GameObject>,
                 ori: Orientation(0.0, 0.0),
                 mesh: get_player_mesh()
             };
-            broadcast.send(AddObject(playerid, newplayer.pos, newplayer.ori, Player));
+            broadcast.send(ServerCommand::AddObject(playerid, newplayer.pos, newplayer.ori, ObjectType::Player));
             entry.set(newplayer)
         }
         Occupied(entry) => { entry.into_mut() }
@@ -178,12 +179,12 @@ fn manage_world(mut world: HashMap<int, GameObject>,
                 broadcast: Sender<ServerCommand>,
                 player_moves: Receiver<(int, PlayerCommand)>) {
     let broadcast_location = |obj: &GameObject, i: int| {
-        broadcast.send(SetPosition(i, obj.pos));
+        broadcast.send(ServerCommand::SetPosition(i, obj.pos));
     };
     for (playerid, action) in player_moves.iter() {
         drop(get_player(&mut world, playerid, broadcast.clone()));
         match action {
-            MoveForward(delta) => {
+            PlayerCommand::MoveForward(delta) => {
                 println!("Player #{} moves {} units forward", playerid, delta);
                 let player = get_player(&mut world, playerid, broadcast.clone());
                 let Orientation(theta, _) = player.ori;
@@ -191,7 +192,7 @@ fn manage_world(mut world: HashMap<int, GameObject>,
                 println!("P#{} pos: {}", playerid, player.pos);
                 broadcast_location(player, playerid);
             }
-            MoveSideways(delta) => {
+            PlayerCommand::MoveSideways(delta) => {
                 println!("Player #{} moves {} units to their right", playerid, delta);
                 let player = get_player(&mut world, playerid, broadcast.clone());
                 let Orientation(theta, _) = player.ori;
@@ -199,7 +200,7 @@ fn manage_world(mut world: HashMap<int, GameObject>,
                 println!("P#{} pos: {}", playerid, player.pos);
                 broadcast_location(player, playerid);
             }
-            MoveUp(delta) => {
+            PlayerCommand::MoveUp(delta) => {
                 println!("Player #{} moves {} units up", playerid, delta);
                 let player = get_player(&mut world, playerid, broadcast.clone());
                 let Orientation(theta, _) = player.ori;
@@ -207,14 +208,14 @@ fn manage_world(mut world: HashMap<int, GameObject>,
                 println!("P#{} pos: {}", playerid, player.pos);
                 broadcast_location(player, playerid);
             }
-            RotateCamera(Orientation(theta, phi)) => {
+            PlayerCommand::RotateCamera(Orientation(theta, phi)) => {
                 println!("Player #{} rotates by ({}, {})", playerid, theta, phi);
                 let player = &mut get_player(&mut world, playerid, broadcast.clone());
                 player.ori = player.ori + Orientation(theta, phi);
                 println!("P#{} ori: {}", playerid, player.ori);
-                broadcast.send(SetOrientation(playerid, player.ori));
+                broadcast.send(ServerCommand::SetOrientation(playerid, player.ori));
             }
-            Shoot => { println!("Player #{} shoots", playerid); }
+            PlayerCommand::Shoot => { println!("Player #{} shoots", playerid); }
         }
     }
 }
@@ -278,10 +279,10 @@ fn main() {
     //let mut receive_broadcast = ReceiverMultiplexer::new(receive_broadcast_precursor);
     let transmitters = Arc::new(Mutex::new(vec!()));
     let transmitters2 = transmitters.clone();
-    spawn(proc() { rebroadcast_transmitter(receive_broadcast_precursor, transmitters2); });
+    spawn(move || { rebroadcast_transmitter(receive_broadcast_precursor, transmitters2); });
 
-    spawn(proc() { manage_world(world, transmit_broadcast, receive_playmove); });
-    //spawn(proc() { receive_broadcast.rebroadcast(); });
+    spawn(move || { manage_world(world, transmit_broadcast, receive_playmove); });
+    //spawn(move || { receive_broadcast.rebroadcast(); });
     
 
     let mut acceptor = listener.listen();
@@ -300,8 +301,8 @@ fn main() {
                     drop(val);
                 }
                 //let rbc = receive_broadcast.clone();
-                //spawn(proc() { show_examples(stream, playernum.clone(), tpm); });
-                spawn(proc() { interact_with_client(stream, playernum, rx, tpm); });
+                //spawn(move || { show_examples(stream, playernum.clone(), tpm); });
+                spawn(move || { interact_with_client(stream, playernum, rx, tpm); });
             }
         }
     }
